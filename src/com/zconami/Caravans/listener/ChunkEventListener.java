be5f2bcd48b1875e0a0d6ea8b708da293bf5076b
Jonny Power <jonny@sunguru.co>
2015-09-06 00:15:32 +0100

    ChunkEventListener (missing from previous commit)

---
+++ src/com/zconami/Caravans/listener/ChunkEventListener.java
@@ -0,0 +1,52 @@
+package com.zconami.Caravans.listener;
+
+import org.bukkit.Chunk;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Horse;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+
+import com.zconami.Caravans.domain.Caravan;
+import com.zconami.Caravans.repository.CaravanRepository;
+import com.zconami.Caravans.util.CaravansUtils;
+import com.zconami.Caravans.util.EntityUtils;
+
+public class ChunkEventListener implements Listener {
+
+    // ===================================
+    // ATTRIBUTES
+    // ===================================
+
+    private final CaravanRepository caravanRepository;
+
+    // ===================================
+    // CONSTRUCTORS
+    // ===================================
+
+    public ChunkEventListener(CaravanRepository caravanRepository) {
+        this.caravanRepository = caravanRepository;
+    }
+
+    // ===================================
+    // PUBLIC METHODS
+    // ===================================
+
+    @EventHandler
+    public void onChunkUnload(ChunkUnloadEvent event) {
+        final Chunk chunk = event.getChunk();
+        for (Entity entity : chunk.getEntities()) {
+            if (CaravansUtils.isCaravan(entity)) {
+                final Caravan caravan = caravanRepository.find((Horse) entity);
+                caravan.setUnloaded(true);
+            }
+            EntityUtils.removeFromCache(entity.getUniqueId().toString());
+        }
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+    }
+
+}
